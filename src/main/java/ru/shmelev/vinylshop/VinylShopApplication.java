cbea139fccca219456b2a5196f3eccc1ff16e8cc
Savva <savahits@gmail.com>
2026-02-16 23:35:44 +0300

    Initialize Spring REST API app VinylShop

---
+++ src/main/java/ru/shmelev/vinylshop/VinylShopApplication.java
@@ -0,0 +1,13 @@
+package ru.shmelev.vinylshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylShopApplication.class, args);
+	}
+
+}
