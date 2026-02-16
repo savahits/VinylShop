d4811d63b6e8ea75f207b5ca37f506146cc0ca4d
Savva <savahits@gmail.com>
2026-02-17 00:53:05 +0300

    feat: create Artist entity

---
+++ src/main/java/ru/shmelev/vinylshop/domain/Artist.java
@@ -0,0 +1,44 @@
+package ru.shmelev.vinylshop.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "artists")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private List<DiscographyItem> discography = new ArrayList<>();
+
+    @OneToMany(mappedBy = "artist")
+    private List<Product> products = new ArrayList<>();
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DiscographyItem {
+        private String albumTitle;
+        private Integer year;
+
+    }
+}
