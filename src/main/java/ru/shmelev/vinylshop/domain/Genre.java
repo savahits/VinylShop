6600975aac75ea10576359b4740a0327db364668
Savva <savahits@gmail.com>
2026-02-17 00:52:48 +0300

    feat: create Genre entity

---
+++ src/main/java/ru/shmelev/vinylshop/domain/Genre.java
@@ -0,0 +1,25 @@
+package ru.shmelev.vinylshop.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<Product> products = new HashSet<>();
+}
