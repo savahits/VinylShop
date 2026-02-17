6acaf9c8577bfca773bdeb3f252a5f26233b54fc
Savva <savahits@gmail.com>
2026-02-18 00:19:42 +0300

    feat: create getGenresByArtistId method for ArtistService

--- src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
+++ src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
@@ -6,21 +6,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.shmelev.vinylshop.DTO.ArtistShowDTO;
 import ru.shmelev.vinylshop.DTO.MultipleArtistsShowDTO;
+import ru.shmelev.vinylshop.domain.Genre;
 import ru.shmelev.vinylshop.mappers.ArtistToDtoMapper;
 import ru.shmelev.vinylshop.repository.ArtistRepository;
+import ru.shmelev.vinylshop.repository.GenreRepository;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class ArtistService {
 
     private final ArtistRepository artistRepository;
     private final ArtistToDtoMapper artistToDtoMapper;
+    private final GenreRepository genreRepository;
 
     @Autowired
-    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper) {
+    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper, GenreRepository genreRepository) {
         this.artistRepository = artistRepository;
         this.artistToDtoMapper = artistToDtoMapper;
+        this.genreRepository = genreRepository;
     }
 
     public List<MultipleArtistsShowDTO> getAllArtists() {
@@ -39,5 +44,13 @@ public class ArtistService {
         }
         artistRepository.deleteById(id);
     }
+
+    public Set<Genre> getGenresByArtistId(Long artistId) {
+        if (!artistRepository.existsById(artistId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Такого артиста нет!");
+        }
+        return genreRepository.findGenresByArtistId(artistId);
+    }
+
 
 }
