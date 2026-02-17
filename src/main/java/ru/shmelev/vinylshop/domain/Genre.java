f370ac05020b07be82b99f60677c79ed327257b8
Savva <savahits@gmail.com>
2026-02-18 00:18:28 +0300

    refactor: refactor annotations for Genre

--- src/main/java/ru/shmelev/vinylshop/domain/Genre.java
+++ src/main/java/ru/shmelev/vinylshop/domain/Genre.java
@@ -2,12 +2,15 @@ package ru.shmelev.vinylshop.domain;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "genres")
