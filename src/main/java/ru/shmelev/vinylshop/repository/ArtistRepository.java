eb64894e21fe0da0968039c4e509071565743318
Shmelev Savelii <savahits@gmail.com>
2026-02-17 10:28:48 +0300

    refactor: add @Repository annotation to ArtistRepository

--- src/main/java/ru/shmelev/vinylshop/repository/ArtistRepository.java
+++ src/main/java/ru/shmelev/vinylshop/repository/ArtistRepository.java
@@ -1,7 +1,9 @@
 package ru.shmelev.vinylshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.shmelev.vinylshop.domain.Artist;
 
+@Repository
 public interface ArtistRepository  extends JpaRepository<Long, Artist> {
 }
