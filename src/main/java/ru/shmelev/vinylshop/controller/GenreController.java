dc8c48c16a6ffe6c5729c015d2d7e8176a96ab78
Savva <savahits@gmail.com>
2026-02-18 00:34:07 +0300

    feat: create GenreController with getAllGenres method

---
+++ src/main/java/ru/shmelev/vinylshop/controller/GenreController.java
@@ -0,0 +1,27 @@
+package ru.shmelev.vinylshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.shmelev.vinylshop.DTO.GenreResponseDTO;
+import ru.shmelev.vinylshop.service.GenreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/genres")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public List<GenreResponseDTO> getAllGenres() {
+        return genreService.findAll();
+    }
+}
