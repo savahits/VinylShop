e8828167e91f7b08c6e021a194ff1b012be0272a
Savva <savahits@gmail.com>
2026-02-17 17:41:12 +0300

    feat: create ArtistService

---
+++ src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
@@ -0,0 +1,27 @@
+package ru.shmelev.vinylshop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.shmelev.vinylshop.DTO.ArtistsShowDTO;
+import ru.shmelev.vinylshop.mappers.ArtistToDtoMapper;
+import ru.shmelev.vinylshop.repository.ArtistRepository;
+
+import java.util.List;
+
+@Service
+public class ArtistService {
+
+    private final ArtistRepository artistRepository;
+    private final ArtistToDtoMapper artistToDtoMapper;
+
+    @Autowired
+    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper) {
+        this.artistRepository = artistRepository;
+        this.artistToDtoMapper = artistToDtoMapper;
+    }
+
+    public List<ArtistsShowDTO> getAllArtists() {
+        return artistToDtoMapper.toShowDTOList(artistRepository.findAll());
+    }
+
+}
