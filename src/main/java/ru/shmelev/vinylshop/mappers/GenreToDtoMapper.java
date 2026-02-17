245191cf3cbe93b2113a3f711bde2915294cdc6c
Savva <savahits@gmail.com>
2026-02-18 00:33:20 +0300

    feat: create GenreToDtoMapper

---
+++ src/main/java/ru/shmelev/vinylshop/mappers/GenreToDtoMapper.java
@@ -0,0 +1,34 @@
+package ru.shmelev.vinylshop.mappers;
+
+import org.springframework.stereotype.Component;
+import ru.shmelev.vinylshop.domain.Genre;
+import ru.shmelev.vinylshop.DTO.GenreResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class GenreToDtoMapper {
+
+    public GenreResponseDTO convert(Genre genre) {
+        if (genre == null) {
+            return null;
+        }
+
+        return new GenreResponseDTO(
+                genre.getId(),
+                genre.getName()
+        );
+    }
+
+    public List<GenreResponseDTO> convert(List<Genre> genres) {
+        if (genres == null) {
+            return List.of();
+        }
+
+        return genres.stream()
+                .map(this::convert)
+                .collect(Collectors.toList());
+    }
+
+}
