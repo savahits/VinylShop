1edce6abbb7525a43222554ecf3a5ba84e4d80bf
Savva <savahits@gmail.com>
2026-02-17 17:38:49 +0300

    feat: add db connection

--- src/main/java/ru/shmelev/vinylshop/VinylShopApplication.java
+++ src/main/java/ru/shmelev/vinylshop/VinylShopApplication.java
@@ -2,8 +2,10 @@ package ru.shmelev.vinylshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:database.properties")
 public class VinylShopApplication {
 
 	public static void main(String[] args) {
