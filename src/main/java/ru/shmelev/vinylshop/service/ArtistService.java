d0844efd3df9211e20e8b026067b6f3a9dc54d55
Shmelev Savelii <savahits@gmail.com>
2026-02-18 17:33:25 +0300

    feat: create getArtistVinyls method for ArtistService

--- src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
+++ src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
@@ -6,13 +6,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.shmelev.vinylshop.DTO.ArtistShowDTO;
 import ru.shmelev.vinylshop.DTO.MultipleArtistsShowDTO;
+import ru.shmelev.vinylshop.DTO.MultipleVinylShowDTO;
 import ru.shmelev.vinylshop.domain.Genre;
+import ru.shmelev.vinylshop.domain.Product;
 import ru.shmelev.vinylshop.mappers.ArtistToDtoMapper;
 import ru.shmelev.vinylshop.repository.ArtistRepository;
 import ru.shmelev.vinylshop.repository.GenreRepository;
+import ru.shmelev.vinylshop.repository.ProductRepository;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ArtistService {
@@ -20,12 +24,14 @@ public class ArtistService {
     private final ArtistRepository artistRepository;
     private final ArtistToDtoMapper artistToDtoMapper;
     private final GenreRepository genreRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper, GenreRepository genreRepository) {
+    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper, GenreRepository genreRepository,  ProductRepository productRepository) {
         this.artistRepository = artistRepository;
         this.artistToDtoMapper = artistToDtoMapper;
         this.genreRepository = genreRepository;
+        this.productRepository = productRepository;
     }
 
     public List<MultipleArtistsShowDTO> getAllArtists() {
@@ -52,5 +58,29 @@ public class ArtistService {
         return genreRepository.findGenresByArtistId(artistId);
     }
 
+    public List<MultipleVinylShowDTO> getArtistVinyls(Long artistId) {
+        if (!artistRepository.existsById(artistId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Такого артиста нет!");
+        }
 
+        List<Product> vinyls = productRepository.findAllVinylsByArtistIdWithGenres(artistId);
+
+        return vinyls.stream()
+                .map(this::mapToVinylShowDTO)
+                .collect(Collectors.toList());
+    }
+
+    private MultipleVinylShowDTO mapToVinylShowDTO(Product product) {
+        List<String> genreNames = product.getGenres().stream()
+                .map(Genre::getName)
+                .collect(Collectors.toList());
+
+        return new MultipleVinylShowDTO(
+                product.getTitle(),
+                product.getArtist().getNickname(),
+                genreNames,
+                product.getPrice()
+        );
+
+    }
 }
