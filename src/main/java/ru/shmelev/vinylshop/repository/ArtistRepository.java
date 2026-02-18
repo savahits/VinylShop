1596bd1ee47830dfc62153ba18aa16bca31267a4
Savva <savahits@gmail.com>
2026-02-18 23:42:21 +0300

    feat: add findByGenresId method for ArtistRepository

--- src/main/java/ru/shmelev/vinylshop/repository/ArtistRepository.java
+++ src/main/java/ru/shmelev/vinylshop/repository/ArtistRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.vinylshop.domain.Artist;
 
+import java.util.List;
+
 @Repository
 public interface ArtistRepository  extends JpaRepository<Artist, Long> {
+    List<Artist> findByGenresId(Long genreId);
 }
