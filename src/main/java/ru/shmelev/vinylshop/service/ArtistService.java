431f6263bfb99d242e4e34ea6c30e649c2e13467
Savva <savahits@gmail.com>
2026-02-21 21:47:30 +0300

    refactor: refactor methods between services

--- src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
+++ src/main/java/ru/shmelev/vinylshop/service/ArtistService.java
@@ -3,36 +3,42 @@ package ru.shmelev.vinylshop.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import ru.shmelev.vinylshop.DTO.ArtistShowDTO;
-import ru.shmelev.vinylshop.DTO.GenreResponseDTO;
 import ru.shmelev.vinylshop.DTO.MultipleArtistsShowDTO;
-import ru.shmelev.vinylshop.DTO.MultipleVinylShowDTO;
 import ru.shmelev.vinylshop.domain.Artist;
-import ru.shmelev.vinylshop.domain.Genre;
-import ru.shmelev.vinylshop.domain.Product;
 import ru.shmelev.vinylshop.mappers.ArtistToDtoMapper;
 import ru.shmelev.vinylshop.repository.ArtistRepository;
-import ru.shmelev.vinylshop.repository.ProductRepository;
+import ru.shmelev.vinylshop.repository.GenreRepository;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class ArtistService {
 
     private final ArtistRepository artistRepository;
     private final ArtistToDtoMapper artistToDtoMapper;
-    private final ProductRepository productRepository;
+    private final GenreRepository genreRepository;
 
     @Autowired
-    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper, ProductRepository productRepository) {
+    public ArtistService(ArtistRepository artistRepository, ArtistToDtoMapper artistToDtoMapper,
+                         GenreRepository genreRepository) {
         this.artistRepository = artistRepository;
         this.artistToDtoMapper = artistToDtoMapper;
-        this.productRepository = productRepository;
+        this.genreRepository = genreRepository;
     }
 
+     @Transactional(readOnly = true)
+    public List<MultipleArtistsShowDTO> getArtistsByGenreId(Long genreId) {
+        if (!genreRepository.existsById(genreId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Жанр не найден");
+        }
+
+        List<Artist> artists = artistRepository.findByGenresId(genreId);
+        return artistToDtoMapper.toShowDTOList(artists);
+
+    }
     public List<MultipleArtistsShowDTO> getAllArtists() {
         return artistToDtoMapper.toShowDTOList(artistRepository.findAll());
     }
@@ -50,38 +56,4 @@ public class ArtistService {
         artistRepository.deleteById(id);
     }
 
-    public Set<GenreResponseDTO> getGenresByArtistId(Long artistId) {
-        Artist artist = artistRepository.findById(artistId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Такого артиста нет!"));
-
-        return artist.getGenres().stream()
-                .map(genre -> new GenreResponseDTO(genre.getId(), genre.getName()))
-                .collect(Collectors.toSet());
-    }
-
-    public List<MultipleVinylShowDTO> getArtistVinyls(Long artistId) {
-        if (!artistRepository.existsById(artistId)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Такого артиста нет!");
-        }
-
-        List<Product> vinyls = productRepository.findAllVinylsByArtistIdWithGenres(artistId);
-
-        return vinyls.stream()
-                .map(this::mapToVinylShowDTO)
-                .collect(Collectors.toList());
-    }
-
-    private MultipleVinylShowDTO mapToVinylShowDTO(Product product) {
-        List<String> genreNames = product.getGenres().stream()
-                .map(Genre::getName)
-                .collect(Collectors.toList());
-
-        return new MultipleVinylShowDTO(
-                product.getTitle(),
-                product.getArtist().getNickname(),
-                genreNames,
-                product.getPrice()
-        );
-
-    }
 }
--- src/main/java/ru/shmelev/vinylshop/service/GenreService.java
+++ src/main/java/ru/shmelev/vinylshop/service/GenreService.java
@@ -3,10 +3,8 @@ package ru.shmelev.vinylshop.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import ru.shmelev.vinylshop.DTO.GenreResponseDTO;
-import ru.shmelev.vinylshop.DTO.MultipleArtistsShowDTO;
 import ru.shmelev.vinylshop.domain.Artist;
 import ru.shmelev.vinylshop.domain.Genre;
 import ru.shmelev.vinylshop.mappers.ArtistToDtoMapper;
@@ -15,6 +13,8 @@ import ru.shmelev.vinylshop.repository.ArtistRepository;
 import ru.shmelev.vinylshop.repository.GenreRepository;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class GenreService {
@@ -22,30 +22,17 @@ public class GenreService {
     private final GenreRepository genreRepository;
     private final GenreToDtoMapper genreToDtoMapper;
     private final ArtistRepository artistRepository;
-    private final ArtistToDtoMapper artistToDtoMapper;
 
     @Autowired
-    public GenreService(GenreRepository genreRepository, GenreToDtoMapper genreToDtoMapper, ArtistRepository artistRepository
-    , ArtistToDtoMapper artistToDtoMapper) {
+    public GenreService(GenreRepository genreRepository, GenreToDtoMapper genreToDtoMapper, ArtistRepository artistRepository) {
         this.genreRepository = genreRepository;
         this.genreToDtoMapper = genreToDtoMapper;
         this.artistRepository = artistRepository;
-        this.artistToDtoMapper = artistToDtoMapper;
     }
 
     public List<GenreResponseDTO> findAll() {
         List<Genre> genres = genreRepository.findAll();
         return genreToDtoMapper.convert(genres);
-    }
-
-    @Transactional(readOnly = true)
-    public List<MultipleArtistsShowDTO> getArtistsByGenreId(Long genreId) {
-        if (!genreRepository.existsById(genreId)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Жанр не найден");
-        }
-
-        List<Artist> artists = artistRepository.findByGenresId(genreId);
-        return artistToDtoMapper.toShowDTOList(artists);
     }
 
     public void deleteGenreById(Long genreId) {
@@ -54,6 +41,15 @@ public class GenreService {
         }
 
         genreRepository.deleteById(genreId);
+    }
+
+    public Set<GenreResponseDTO> getGenresByArtistId(Long artistId) {
+        Artist artist = artistRepository.findById(artistId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Такого артиста нет!"));
+
+        return artist.getGenres().stream()
+                .map(genre -> new GenreResponseDTO(genre.getId(), genre.getName()))
+                .collect(Collectors.toSet());
     }
 
 }
