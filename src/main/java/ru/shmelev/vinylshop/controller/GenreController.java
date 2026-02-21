3800ac58c428888f1cd3eea0a1168d88fda4025c
Savva <savahits@gmail.com>
2026-02-21 21:49:41 +0300

    feat: getVinylsByGenre method for GenreController

--- src/main/java/ru/shmelev/vinylshop/controller/GenreController.java
+++ src/main/java/ru/shmelev/vinylshop/controller/GenreController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.shmelev.vinylshop.DTO.GenreResponseDTO;
 import ru.shmelev.vinylshop.DTO.MultipleArtistsShowDTO;
+import ru.shmelev.vinylshop.DTO.MultipleVinylShowDTO;
+import ru.shmelev.vinylshop.service.ArtistService;
 import ru.shmelev.vinylshop.service.GenreService;
+import ru.shmelev.vinylshop.service.VinylService;
 
 import java.util.List;
 
@@ -13,10 +16,14 @@ import java.util.List;
 public class GenreController {
 
     private final GenreService genreService;
+    private final ArtistService artistService;
+    private final VinylService vinylService;
 
     @Autowired
-    public GenreController(GenreService genreService) {
+    public GenreController(GenreService genreService, ArtistService artistService, VinylService vinylService) {
         this.genreService = genreService;
+        this.artistService = artistService;
+        this.vinylService = vinylService;
     }
 
     @GetMapping
@@ -32,6 +39,11 @@ public class GenreController {
 
     @GetMapping("/{id}/artists")
     public List<MultipleArtistsShowDTO> getArtistsByGenre(@PathVariable Long id) {
-        return genreService.getArtistsByGenreId(id);
+        return artistService.getArtistsByGenreId(id);
+    }
+
+    @GetMapping("/{id}/vinyls")
+    public List<MultipleVinylShowDTO> getVinylsByGenre(@PathVariable Long id) {
+        return vinylService.getVinylsByGenre(id);
     }
 }
