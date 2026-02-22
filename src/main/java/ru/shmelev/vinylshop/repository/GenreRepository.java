a25f922fb7084ea7f7b8caed6a056c60238a615b
Savva <savahits@gmail.com>
2026-02-22 18:53:21 +0300

    feat: add existsByName for GenreRepository

--- src/main/java/ru/shmelev/vinylshop/repository/GenreRepository.java
+++ src/main/java/ru/shmelev/vinylshop/repository/GenreRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.vinylshop.domain.Genre;
 
-import java.util.Set;
-
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
-
+    boolean existsByName(String name);
 }
