af5f08e13cbeb9721b9a7db86bbc309340b32328
Savva <savahits@gmail.com>
2026-02-24 15:34:08 +0300

    fix: add JpaRepository extension for CDRepository

--- src/main/java/ru/shmelev/vinylshop/repository/CDRepository.java
+++ src/main/java/ru/shmelev/vinylshop/repository/CDRepository.java
@@ -1,5 +1,6 @@
 package ru.shmelev.vinylshop.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.vinylshop.domain.Product;
@@ -7,7 +8,7 @@ import ru.shmelev.vinylshop.domain.Product;
 import java.util.List;
 
 @Repository
-public interface CDRepository {
+public interface CDRepository extends JpaRepository<Product, Long> {
 
     @Query("select p from Product p where p.format = 'cd'")
     List<Product> findAllCD();
