fb2ab5058707bd8f9cc44a8a30d0f2dab09ec757
Savva <savahits@gmail.com>
2026-02-24 15:56:58 +0300

    feat: MultipleVinylShowDTO response List<GenreResponseDTO> genres instead of List<String>

--- src/main/java/ru/shmelev/vinylshop/DTO/vinyl/MultipleVinylShowDTO.java
+++ src/main/java/ru/shmelev/vinylshop/DTO/vinyl/MultipleVinylShowDTO.java
@@ -1,5 +1,7 @@
 package ru.shmelev.vinylshop.DTO.vinyl;
 
+import ru.shmelev.vinylshop.DTO.genre.GenreResponseDTO;
+
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -7,7 +9,7 @@ public record MultipleVinylShowDTO(
         Long id,
         String title,
         String artistName,
-        List<String> genres,
+        List<GenreResponseDTO> genres,
         BigDecimal price
 ) {
 }
