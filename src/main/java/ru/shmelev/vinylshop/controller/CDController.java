c53dfd54a2c2bc53555229711c3d6c520f522cfe
Savva <savahits@gmail.com>
2026-02-24 19:37:04 +0300

    feat: add getCDById method for CDController

--- src/main/java/ru/shmelev/vinylshop/controller/CDController.java
+++ src/main/java/ru/shmelev/vinylshop/controller/CDController.java
@@ -2,8 +2,10 @@ package ru.shmelev.vinylshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.shmelev.vinylshop.DTO.CD.CDShowDTO;
 import ru.shmelev.vinylshop.DTO.CD.MultipleCDShowDTO;
 import ru.shmelev.vinylshop.service.CDService;
 
@@ -22,6 +24,11 @@ public class CDController {
 
     @GetMapping
     public List<MultipleCDShowDTO> getCDs() {
-        return cdService.findAllCD();
+        return cdService.getAllCD();
+    }
+
+    @GetMapping("/{id}")
+    public CDShowDTO getCDById(@PathVariable Long id) {
+        return cdService.getCDById(id);
     }
 }
