d756b0f54cff9ba350b06ecc0ffb019614610fe7
Savva <savahits@gmail.com>
2026-02-25 18:49:21 +0300

    feat: add description to ArtistController methods for swagger documentation

--- src/main/java/ru/shmelev/vinylshop/controller/ArtistController.java
+++ src/main/java/ru/shmelev/vinylshop/controller/ArtistController.java
@@ -1,5 +1,6 @@
 package ru.shmelev.vinylshop.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -30,16 +31,22 @@ public class ArtistController {
     }
 
     @GetMapping
+    @Operation(summary = "Получить всех возможных артистов",
+            description = "Возвращает список всех существующих артистов")
     public List<MultipleArtistsShowDTO> getArtists() {
         return artistService.getAllArtists();
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Получить информацию про конкретного артиста",
+            description = "Возвращает всю информацию про данного артиста")
     public ArtistShowDTO getArtist(@PathVariable Long id) {
         return artistService.getArtistById(id);
     }
 
     @GetMapping("{id}/vinyls")
+    @Operation(summary = "Получить все винилы данного артиста",
+            description = "Возвращает все винилы данного артиста")
     public List<MultipleVinylShowDTO> getVinyls(@PathVariable Long id) {
         return vinylService.getArtistVinyls(id);
     }
@@ -50,6 +57,8 @@ public class ArtistController {
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Удалить артиста",
+            description = "Удаляет данного артиста, ничего не возвращает")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteArtist(@PathVariable Long id) {
         artistService.deleteArtistById(id);
