bf6d963c7f84b7aff199e06a06d02df57d1e7805
Savva <savahits@gmail.com>
2026-02-25 18:54:48 +0300

    feat: delete getGenresByArtistId method from ArtistController

--- src/main/java/ru/shmelev/vinylshop/controller/ArtistController.java
+++ src/main/java/ru/shmelev/vinylshop/controller/ArtistController.java
@@ -5,28 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.shmelev.vinylshop.DTO.artist.ArtistShowDTO;
-import ru.shmelev.vinylshop.DTO.genre.GenreResponseDTO;
 import ru.shmelev.vinylshop.DTO.artist.MultipleArtistsShowDTO;
 import ru.shmelev.vinylshop.DTO.vinyl.MultipleVinylShowDTO;
 import ru.shmelev.vinylshop.service.ArtistService;
-import ru.shmelev.vinylshop.service.GenreService;
 import ru.shmelev.vinylshop.service.VinylService;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/artists")
 public class ArtistController {
 
     private final ArtistService artistService;
-    private final GenreService genreService;
     private final VinylService vinylService;
 
     @Autowired
-    public ArtistController(ArtistService artistService, GenreService genreService, VinylService vinylService) {
+    public ArtistController(ArtistService artistService, VinylService vinylService) {
         this.artistService = artistService;
-        this.genreService = genreService;
         this.vinylService = vinylService;
     }
 
@@ -49,11 +44,6 @@ public class ArtistController {
             description = "Возвращает все винилы данного артиста")
     public List<MultipleVinylShowDTO> getVinyls(@PathVariable Long id) {
         return vinylService.getArtistVinyls(id);
-    }
-
-    @GetMapping("/{id}/genres")
-    public Set<GenreResponseDTO> getGenresByArtistId(@PathVariable Long id) {
-        return genreService.getGenresByArtistId(id);
     }
 
     @DeleteMapping("{id}")
