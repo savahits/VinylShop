7bd855d136e43ace95a625ec1787ef64018c35c1
Savva <savahits@gmail.com>
2026-02-25 19:15:34 +0300

    fix: fixed GenreToDtoMapper

--- src/main/java/ru/shmelev/vinylshop/mappers/GenreToDtoMapper.java
+++ src/main/java/ru/shmelev/vinylshop/mappers/GenreToDtoMapper.java
@@ -1,10 +1,11 @@
 package ru.shmelev.vinylshop.mappers;
 
 import org.springframework.stereotype.Component;
-import ru.shmelev.vinylshop.domain.Genre;
 import ru.shmelev.vinylshop.DTO.genre.GenreResponseDTO;
+import ru.shmelev.vinylshop.domain.Genre;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -14,21 +15,24 @@ public class GenreToDtoMapper {
         if (genre == null) {
             return null;
         }
-
-        return new GenreResponseDTO(
-                genre.getId(),
-                genre.getName()
-        );
+        return new GenreResponseDTO(genre.getId(), genre.getName());
     }
 
     public List<GenreResponseDTO> convert(List<Genre> genres) {
         if (genres == null) {
             return List.of();
         }
-
         return genres.stream()
                 .map(this::convert)
                 .collect(Collectors.toList());
     }
 
+    public List<GenreResponseDTO> convert(Set<Genre> genres) {
+        if (genres == null) {
+            return List.of();
+        }
+        return genres.stream()
+                .map(this::convert)
+                .collect(Collectors.toList());
+    }
 }
