a10ca0dd404dbb0177069bd101498a773e367234
Savva <savahits@gmail.com>
2026-02-25 20:04:52 +0300

    fix: change property in MultipleCDShowDTO from String artistName to ArtistDTO artist

--- src/main/java/ru/shmelev/vinylshop/DTO/CD/MultipleCDShowDTO.java
+++ src/main/java/ru/shmelev/vinylshop/DTO/CD/MultipleCDShowDTO.java
@@ -1,5 +1,6 @@
 package ru.shmelev.vinylshop.DTO.CD;
 
+import ru.shmelev.vinylshop.DTO.artist.ArtistDTO;
 import ru.shmelev.vinylshop.DTO.genre.GenreResponseDTO;
 
 import java.math.BigDecimal;
@@ -8,7 +9,7 @@ import java.util.List;
 public record MultipleCDShowDTO (
         Long id,
         String title,
-        String artistName,
+        ArtistDTO artist,
         List<GenreResponseDTO> genres,
         BigDecimal price
 ){
