fb95cbc87b9ce577fcecf110a54c724499464396
Savva <savahits@gmail.com>
2026-02-27 00:12:58 +0300

    feat: findByGenresId returns Page<Artist>

--- src/main/java/ru/shmelev/vinylshop/repository/ArtistRepository.java
+++ src/main/java/ru/shmelev/vinylshop/repository/ArtistRepository.java
@@ -1,6 +1,10 @@
 package ru.shmelev.vinylshop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.shmelev.vinylshop.domain.Artist;
 
@@ -8,5 +12,11 @@ import java.util.List;
 
 @Repository
 public interface ArtistRepository  extends JpaRepository<Artist, Long> {
+
+    @Deprecated
     List<Artist> findByGenresId(Long genreId);
+
+    @Query(value = "SELECT a FROM Artist a JOIN a.genres g WHERE g.id = :genreId",
+            countQuery = "SELECT COUNT(a) FROM Artist a JOIN a.genres g WHERE g.id = :genreId")
+    Page<Artist> findByGenresId(@Param("genreId") Long genreId, Pageable pageable);
 }
