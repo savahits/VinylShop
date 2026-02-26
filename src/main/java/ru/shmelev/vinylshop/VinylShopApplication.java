e799d69f42304c12f23b9fcb851e55850d1aff36
Savva <savahits@gmail.com>
2026-02-27 00:25:18 +0300

    feat: add Spring Data's PagedModel globally

--- src/main/java/ru/shmelev/vinylshop/VinylShopApplication.java
+++ src/main/java/ru/shmelev/vinylshop/VinylShopApplication.java
@@ -3,9 +3,13 @@ package ru.shmelev.vinylshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @PropertySource(value = "classpath:database.properties")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class VinylShopApplication {
 
 	public static void main(String[] args) {
